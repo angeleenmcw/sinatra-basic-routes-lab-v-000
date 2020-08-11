c7248ca998a9007772bf8215921be00b3e7a4af9
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-08-11 03:25:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+    
+  get '/name' do
+    @name = Name.all
+  end 
+  
+  get '/hometown' do
+    @hometown = Hometown.all
+  end 
+  
+  get '/favorite-song' do
+    @favorite-song = Favorite-Song.all
+  end 
 end
